58290893f07bd98746af0a9f7d3278e701ba98e1
fabiandatalyst-glitch <fabianpertuz78@gmail.com>
2025-10-07 19:39:29 -0500

    adicion de scatter w boton

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig_hist = px.histogram(car_data, x="odometer")
         
             # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+scatt_button = st.button('Construir diagrama de dispersion') # crear un botón
+     
+if scatt_button: # al hacer clic en el botón
+         # escribir un mensaje
+    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un diagrama de dispersion
+    fig_scatt = px.scatter(car_data, x="odometer", y="price")
+        
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_scatt, use_container_width=True)
